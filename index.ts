fd46db0138f3177a838d95ca2fccbc0b857fca20
ryosuke fukuyama <ryosukemakaritoru425@gmail.com>
2023-02-01 16:34:45 +0900

    14/172 型注釈

--- index.ts
+++ index.ts
@@ -1,2 +1,11 @@
 let hello: string = 'hello';
 console.log(hello);
+
+let hasValue: boolean = true;
+let count: number = 10;
+let float: number = 3.14;
+let negative: number = -0.12;
+let single: string = 'hello';
+let double: string = "hello";
+let back: string = `hello`;
+
