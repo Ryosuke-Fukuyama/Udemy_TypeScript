3666518610a60ac2f8d11a573b98a707f5f2a6a2
ryosuke fukuyama <ryosukemakaritoru425@gmail.com>
2023-02-01 21:31:27 +0900

    31/172 色々な型

--- index.js
+++ index.js
@@ -1,2 +1,52 @@
 var hello = 'hello';
 console.log(hello);
+var hasValue = true;
+var count = 10;
+var float = 3.14;
+var negative = -0.12;
+var single = 'hello';
+var double = "hello";
+var back = "hello";
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+var person = {
+    name: 'Jack',
+    age: 21
+};
+var fruits = ['Apple', 'Banana', 'Grape'];
+var book = ['business', 1500, false];
+var CoffeeSize;
+(function (CoffeeSize) {
+    CoffeeSize[CoffeeSize["SHORT"] = 0] = "SHORT";
+    CoffeeSize[CoffeeSize["TALL"] = 1] = "TALL";
+    CoffeeSize[CoffeeSize["GRANDEE"] = 2] = "GRANDEE";
+    CoffeeSize[CoffeeSize["VENTI"] = 3] = "VENTI";
+})(CoffeeSize || (CoffeeSize = {}));
+var coffee = {
+    hot: true,
+    size: CoffeeSize.TALL
+};
+coffee.size = CoffeeSize.SHORT;
+var anything = true;
+anything = 'hello';
+anything = ['hello', 33, true];
+anything = {};
+var banana;
+banana = anything;
+var unionType = 10;
+// unionType.toUpperCase();
+unionType = 'hello';
+unionType.toUpperCase();
+var unionTypes = [21, 'hello'];
+var apple = 'apple';
+var clothSize = 'large';
+var cloth = {
+    color: 'white',
+    size: clothSize
+};
+var newCloth = {
+    color: 'white',
+    size: 'medium'
+};
